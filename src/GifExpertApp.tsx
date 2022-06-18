08e0ba311f745bd868f1fb5773c1d61934abb322
Lisandro Rocha Tau <lisandro@itcplatform.de>
2022-06-18 19:51:27 +0200

    update docs

--- src/GifExpertApp.tsx
+++ src/GifExpertApp.tsx
@@ -10,7 +10,7 @@ const GifExpertApp = () => {
         <div className="font-sans relative flex min-h-screen flex-col justify-center antialiased text-slate-400 bg-slate-900 p-20">
             <div className="absolute z-20 top-0 inset-x-0 flex justify-center overflow-hidden pointer-events-none">
                 <div className="w-[108rem] flex-none flex justify-end">
-                    <img src={decoImg} alt="" className="w-[71.75rem] flex-none max-w-none"></img>
+                    <img src={`./${decoImg}`} alt="" className="w-[71.75rem] flex-none max-w-none"></img>
                 </div>
             </div>
             <h1 className="mx-auto text-5xl font-medium pb-2">Search GIFs APP</h1>
